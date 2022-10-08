0f61ef02e6ba300577150edbbeb3500514fb4834
tahataha005 <taha.f.taha0@gmail.com>
2022-10-09 01:34:35 +0300

    Frontend: Creating Landing.jsx and its structure elements in Components

---
+++ e-learning-frontend/src/Components/Landing.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { AuthInput } from "./Auth-Input";
+
+const Landing = () => {
+    return (
+        <>
+            <div className="page-content">
+                <div className="authentication-container">
+                    <div className="authentication-container">
+                        <p className="title">Log In</p>
+                        <div className="login-content"></div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Landing;
