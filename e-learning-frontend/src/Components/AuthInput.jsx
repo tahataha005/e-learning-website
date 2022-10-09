5a8d21e9d94625f4c06cb155e2c74d855add77ef
tahataha005 <taha.f.taha0@gmail.com>
2022-10-09 10:37:28 +0300

    Frontend: Adding class to Components/AuthInput.jsx

--- e-learning-frontend/src/Components/AuthInput.jsx
+++ e-learning-frontend/src/Components/AuthInput.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export const AuthInput = ({ placeholder }) => {
-    return <input type="text" placeholder={placeholder}></input>;
+    return (
+        <input
+            className="auth-input"
+            type="text"
+            placeholder={placeholder}
+        ></input>
+    );
 };
