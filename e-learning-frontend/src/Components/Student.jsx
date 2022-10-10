ed2f27c4994a35fb6aca36a71291493be16809d1
tahataha005 <taha.f.taha0@gmail.com>
2022-10-10 21:56:10 +0300

    Frontend: Mapping announcements in announcements wrapper in Stdent.jsx

--- e-learning-frontend/src/Components/Student.jsx
+++ e-learning-frontend/src/Components/Student.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Announcement from "./Announcement";
 import "../styles/Student.css";
 import Header from "./Header";
 import SideBar from "./SideBar";
@@ -11,6 +12,21 @@ const Student = () => {
         { id: 4, name: "English" },
         { id: 5, name: "English" },
         { id: 6, name: "English" },
+    ];
+
+    const announcements = [
+        {
+            id: 1,
+            title: "Announcement 1",
+            content:
+                "Text Text Text Text Text Text Text Text Text Text Text Text",
+        },
+        { id: 2, title: "Announcement 2", content: "Text Text Text" },
+        { id: 3, title: "Announcement 3", content: "Text Text Text" },
+        { id: 4, title: "Announcement 4", content: "Text Text Text" },
+        { id: 5, title: "Announcement 5", content: "Text Text Text" },
+        { id: 5, title: "Announcement 5", content: "Text Text Text" },
+        { id: 5, title: "Announcement 5", content: "Text Text Text" },
     ];
 
     const userstamp = ["Username", "Student"];
@@ -30,7 +46,17 @@ const Student = () => {
                         <div className="announcements-title flex">
                             <p className="subtitle dark-txt">Announcements</p>
                         </div>
-                        <div className="announcements-wrapper flex column"></div>
+                        <div className="announcements-wrapper flex column">
+                            {announcements.map(announcement => {
+                                return (
+                                    <Announcement
+                                        key={announcement.id}
+                                        title={announcement.title}
+                                        content={announcement.content}
+                                    />
+                                );
+                            })}
+                        </div>
                     </div>
                 </div>
             </div>
