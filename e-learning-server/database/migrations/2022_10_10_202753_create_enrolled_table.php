5594d61f2ca967a6505f372bbebed5f5483fe83e
tahataha005 <taha.f.taha0@gmail.com>
2022-10-10 23:53:58 +0300

    Backend: Creating enrolled table migration in migrations

---
+++ e-learning-server/database/migrations/2022_10_10_202753_create_enrolled_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enrolled', function (Blueprint $table) {
+            $table->foreignId('student_id')->constrained()->references('id')->on('users');
+            $table->foreignId('course_id')->constrained()->references('id')->on('courses');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enrolled');
+    }
+};
