1f7c8f55e18a0ef7018323307cc66b4ebf097f0b
tahataha005 <taha.f.taha0@gmail.com>
2022-10-10 10:40:14 +0300

    Frontend: Adding SideBar component to Student.jsx

--- e-learning-frontend/src/Components/Student.jsx
+++ e-learning-frontend/src/Components/Student.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import "../styles/Student.css";
+import SideBar from "./SideBar";
 
 const Student = () => {
-    return <div>Student</div>;
+    const buttons = [
+        { id: 1, name: "Electronics" },
+        { id: 2, name: "Math" },
+        { id: 3, name: "English" },
+        { id: 4, name: "English" },
+        { id: 5, name: "English" },
+        { id: 6, name: "English" },
+    ];
+
+    const userstamp = ["Username", "Student"];
+
+    return (
+        <div className="page-container flex white-bg">
+            <SideBar
+                buttons={buttons}
+                btn_class={"course-btn"}
+                userstamp={userstamp}
+                side_class={"light-bg"}
+            />
+        </div>
+    );
 };
 
 export default Student;
