151ab405d7fcfcf73272c326c390833a9453c0e6
tahataha005 <taha.f.taha0@gmail.com>
2022-10-11 09:25:28 +0300

    Backend: Creating getCourse function in UserConotroller

--- e-learning-server/app/Http/Controllers/UserController.php
+++ e-learning-server/app/Http/Controllers/UserController.php
@@ -4,12 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Course;
+
 
 class UserController extends Controller
 {
-    function getUser($id){
-        $user=User::find($id);
+    //Function that gets user info from id
+    function getUser($user_id){
+        $user=User::find($user_id);
 
         return response()->json(["user"=>$user]);
+    }
+
+    function getCourse($course_id){
+        $course=Course::find($course_id);
+
+        return response()->json(["user"=>$course]);
     }
 }
