f5a82e0d85f23e15419e01e838e340607df966d5
tahataha005 <taha.f.taha0@gmail.com>
2022-10-11 20:13:39 +0300

    Backend: Creating addAnnouncement function in InstructorController

--- e-learning-server/app/Http/Controllers/InstructorController.php
+++ e-learning-server/app/Http/Controllers/InstructorController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Course;
+use App\Models\Announcement;
+
 
 class InstructorController extends Controller
 {
-    //
+    function addAnnouncement(Request $request){
+        $announcement = Announcement::create([
+            "title"=>$request->title,
+            "content"=>$request->content,
+            "assignment"=>$request->assignment,
+            "course_id"=>$request->course_id,
+        ]);
+
+        return response()->json([
+            "announcement"=>$announcement,
+            "status"=>"announcement posted"
+        ]);
+    }
 }
