a5c61a53d3cb06efca2d4449e11d99abbef4f310
tahataha005 <taha.f.taha0@gmail.com>
2022-10-11 17:38:47 +0300

    Backend: Editing addField function logic in AdminController

--- e-learning-server/app/Http/Controllers/AdminController.php
+++ e-learning-server/app/Http/Controllers/AdminController.php
@@ -29,49 +29,69 @@ class AdminController extends Controller
         return response()->json(["status"=>"Not valid request"]);
     }
 
+    //Adding a specified field
     function addField(Request $request){
+        
+        //Instructor or student check
+        if($request->username){
+            $fetch_username = User::where("username",$request->username)->get();
 
-        $fetch_username = User::where("username",$request->username)->get();
+            //If not exists allow adding
+            if($fetch_username->isEmpty()){
+                $field = $request->field;
+                
+                //Adding an instructor
+                if($field == "instructor"){
+                    $user = User::create([
+                        "username"=>$request->username,
+                        "password"=> Hash::make($request->password),
+                        "user_type"=>2,
+                    ]);
+                
+                    return response()->json([
+                        "username"=>$request->username,
+                        "status"=>"instructor saved successful"
+                    ]);
+                }
 
-        if($fetch_username->isEmpty()){
-
-            $field = $request->field;
-
-            if($field == "instructor"){
-
-                $user = User::create([
-                    "username"=>$request->username,
-                    "password"=> Hash::make($request->password),
-                    "user_type"=>2,
-                ]);
-    
-                return response()->json([
-                    "username"=>$request->username,
-                    "status"=>"instructor saved successful"
-                ]);
+                //Adding a student                
+                if($field == "student"){
+                    $user = User::create([
+                        "username"=>$request->username,
+                        "password"=> Hash::make($request->password),
+                        "user_type"=>1,
+                    ]);
+                
+                    return response()->json([
+                        "username"=>$request->username,
+                        "status"=>"student saved successful"
+                    ]);
+                }
             }
-            if($field == "student"){
 
-                $user = User::create([
-                    "username"=>$request->username,
-                    "password"=> Hash::make($request->password),
-                    "user_type"=>1,
-                ]);
-    
-                return response()->json([
-                    "username"=>$request->username,
-                    "status"=>"student saved successful"
-                ]);
-            }
-            if($field == "course"){
+            //User exists
+            return response()->json([
+                "status"=>"user already exists"
+            ]);
+        }
 
+        //Course check
+        if($request->course_name){
+
+            $fetch_course = Course::where("name",$request->course_name)->get();
+
+            //Check if course doesn't exist
+            if($fetch_course->isEmpty()){
+
+                //check for instructor name
                 $get_instructor_id = User::select("id")->where("username",$request->instructor_username)->get();
 
                 if($get_instructor_id->isEmpty()){
                     return response()->json(["status"=>"instructor not found"]);
                 }
 
-                $user = Course::create([
+                //If not
+                $course = Course::create([
                     "crn"=>$request->crn,
                     "name"=> $request->name,
                     "time"=>$request->time,
@@ -83,11 +103,16 @@ class AdminController extends Controller
                     "status"=>"course saved successful"
                 ]);
             }
+
+            //If course exists
+            return response()->json([
+                "status"=>"course already exists"
+            ]);
         }
 
+        //If no fild created
         return response()->json([
-            "username"=>$request->username,
-            "status"=>"Username already exists"
-        ]);
+            "status"=>"not a valid field"
+        ]);       
     }
 }
