bcbc796fc64419586f1871025f9df73f361472ae
tahataha005 <taha.f.taha0@gmail.com>
2022-10-11 09:15:48 +0300

    Backend: Grouping authentication routes in routes/api.php

--- e-learning-server/routes/api.php
+++ e-learning-server/routes/api.php
@@ -3,13 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JWTController;
+use App\Http\Controllers\UserController;
 
-Route::post('/register', [JWTController::class, 'register']);
-Route::post('/login', [JWTController::class, 'login']);
+
+Route::group(["prefix"=>"authentication"],function(){
+    Route::post('/register', [JWTController::class, 'register']);
+    Route::post('/login', [JWTController::class, 'login']);
+});
+
+
+
+
 Route::group(['middleware' => 'api'], function($router) {
 
-    Route::post('/logout', [JWTController::class, 'logout']);
-    Route::post('/refresh', [JWTController::class, 'refresh']);
-    Route::post('/profile', [JWTController::class, 'profile']);
 });
 
