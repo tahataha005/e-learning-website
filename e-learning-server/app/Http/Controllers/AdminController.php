75d0c59322425490cd0f47e7bfb1d17bf2b47dfa
tahataha005 <taha.f.taha0@gmail.com>
2022-10-11 13:01:03 +0300

    Backend: Creating getRecords function in AdminController

--- e-learning-server/app/Http/Controllers/AdminController.php
+++ e-learning-server/app/Http/Controllers/AdminController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Course;
+use App\Models\Announcement;
+
 
 class AdminController extends Controller
 {
-    //
+    function getRecords($records){
+        if($records == "courses"){
+            $courses = Course::select("*")->get();            
+            return response()->json(["records"=>$courses]);
+        }
+        if($records == "instructors"){
+            $instructors = User::select("*")->where("user_type",2)->get();            
+            return response()->json(["records"=>$instructors]);
+        }
+        if($records == "students"){
+            $students = User::select("*")->where("user_type",1)->get();            
+            return response()->json(["records"=>$students]);
+        }
+        return response()->json(["status"=>"Not valid request"]);
+
+    }
 }
