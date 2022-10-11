fbc17161557174dd070e549c2605103fec2ca523
tahataha005 <taha.f.taha0@gmail.com>
2022-10-11 08:28:33 +0300

    Backend: Editing User model for JWT

--- e-learning-server/app/Models/User.php
+++ e-learning-server/app/Models/User.php
@@ -7,11 +7,28 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+    
+    
     public function userTypes(){
         return $this->belongsTo(User_type::class,"user_type");
     } 
