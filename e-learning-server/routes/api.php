841e40f716a7136c312662d2d63328a89d8bd49f
tahataha005 <taha.f.taha0@gmail.com>
2022-10-11 13:04:52 +0300

    Backend: Creating /admin group and records route to call for getRecords function in AdminController

--- e-learning-server/routes/api.php
+++ e-learning-server/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JWTController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
 
 
 Route::group(["prefix"=>"authentication"],function(){
@@ -14,6 +15,10 @@ Route::group(["prefix"=>"authentication"],function(){
 Route::get("/user/{id}",[UserController::class,"getUser"]);
 Route::get("/course/{id}",[UserController::class,"getCourse"]);
 
+
+Route::group(["prefix"=>"admin"],function(){
+    Route::get("/{records}",[AdminController::class,"getRecords"]);
+});
 
 Route::group(['middleware' => 'api'], function($router) {
 
