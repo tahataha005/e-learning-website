7c372b1ede9fe55dc76d384940d2f2486db247e8
tahataha005 <taha.f.taha0@gmail.com>
2022-10-11 09:13:50 +0300

    Backend: Creating getUser function in UserConotroller

--- e-learning-server/app/Http/Controllers/UserController.php
+++ e-learning-server/app/Http/Controllers/UserController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+    function getUser($id){
+        $user=User::find($id);
+
+        return response()->json(["user"=>$user]);
+    }
 }
